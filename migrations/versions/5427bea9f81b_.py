cfc1842929d40e5281a20ea7978766c52c9ab8f9
juan1lopez <juanilpz86@gmail.com>
2024-08-13 20:37:34 -0300

    first commit

---
+++ migrations/versions/5427bea9f81b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5427bea9f81b
+Revises: 
+Create Date: 2024-05-24 21:22:20.195424
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5427bea9f81b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
